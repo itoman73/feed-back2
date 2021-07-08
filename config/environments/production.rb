e7811b10a1f6d2a8b84fb3be4ccf618f7565ddb0
EC2 Default User <ec2-user@ip-172-31-23-104.us-east-2.compute.internal>
2021-07-08 07:15:38 +0000

    production.rbの修正2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,7 +117,6 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-　  # メール送信関係で追加した内容
 　config.action_mailer.default_url_options = { :host => 'endorphins-feedback2.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   mail = Settings.gmail[:user_name]
